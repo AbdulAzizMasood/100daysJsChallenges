a2c7fda61afa8ef39eb24a7e27d003d81ee9e2da
AbdulAzizMasood <masoodazizabdul@gmail.com>
2023-09-01 12:18:03 +0530

    Toggle between night mode and light mode addedd

---
+++ Day5/orders.js
@@ -0,0 +1,33 @@
+const Orders = [
+    {
+        productName : 'Foldable Mini Drone',
+        productNumber : '83421',
+        paymentStatus : 'Due',
+        shipping : 'Pending'
+    },
+    {
+        productName : 'Larvender KF129 Drone',
+        productNumber : '83323',
+        paymentStatus : 'Refunded',
+        shipping : 'Declined'
+    },
+    {
+        productName : 'Ruko F11 Pro Drone',
+        productNumber : '48929',
+        paymentStatus : 'Due',
+        shipping : 'Pending'
+    },
+    {
+        productName : 'Drone with Camera',
+        productNumber : '45421',
+        paymentStatus : 'Paid',
+        shipping : 'Delivered'
+    },
+    {
+        productName : 'GPS 4k Drone',
+        productNumber : '84551',
+        paymentStatus : 'Paid',
+        shipping : 'Delivered'
+    },
+
+]
--- Day5/script.js
+++ Day5/script.js
@@ -1,9 +1,27 @@
 const sideMenu = document.querySelector("aside");
 const menuBtn = document.querySelector(".menu-btn");
 const closeBtn = document.querySelector(".close-btn");
+const themeToggler = document.querySelector('.theme-toggler');
 menuBtn.addEventListener('click',()=>{
     sideMenu.style.display = `block`;
 })
 closeBtn.addEventListener('click',()=>{
     sideMenu.style.display ='none'
+})
+
+themeToggler.addEventListener('click',()=>{
+    document.body.classList.toggle('dark-theme-variables');
+    themeToggler.querySelector('span:nth-child(1)').classList.toggle('active');
+    themeToggler.querySelector('span:nth-child(2)').classList.toggle('active');
+})
+Orders.forEach(orders=>{
+    const tr = document.createElement('tr');
+    const trData = `
+                 <td>${orders.productName}</td>
+                  <td>${orders.productNumber}</td>
+                 <td>${orders.paymentStatus}</td>
+                 <td class="${orders.shipping ==='Declined' ? 'danger' : orders.shipping ==='Pending' ? 'warning' : 'primary'} ">${orders.shipping}</td>
+                  <td class="primary">Details</td>`;
+      tr.innerHTML = trData;
+      document.querySelector('table tbody').appendChild(tr);            
 })
